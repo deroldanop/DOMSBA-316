6b292f109fc51cd740c7604b57113e655a2bf69d
Roldan Ordaz Paez <roldanop@gmail.com>
2024-02-04 08:46:10 -0800

    readme & rules html

--- index2.js
+++ index2.js
@@ -1,115 +1,107 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Cache elements
     const appBlock = document.getElementById('app');
-    const taskList = document.getElementById('taskList');
-    const taskForm = document.getElementById('taskForm');
-    const newTaskInput = document.querySelector('#newTask');
-    const newCostInput = document.getElementById('newCost');
-    let moneyToSpend = document.getElementById('tospend')
-    let newAmountText = document.getElementById('amount');
-    newAmountText.value = 0;
-    parseFloat(newAmountText);
-    // Event listener for form submission
-    taskForm.addEventListener('submit', function (event) {
-      event.preventDefault();
-      addTask(newTaskInput.value, newCostInput.value);
-      Number(moneyToSpend.value);
+  const taskList = document.getElementById('taskList');
+  const taskForm = document.getElementById('taskForm');
+  let moneyToSpend = document.getElementById('tospend')
+  let newAmountText = document.querySelector('#amount');
+  let notEnoughMoneyText = document.getElementById('not-enough-money');
+  newAmountText.value = 0;
+  // Event listener for form submission
+  taskForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const newTask = taskForm.firstElementChild;
+    const newCost = taskForm.children[1];
 
-      newTaskInput.value = ''; // Clear the input field
-      newCostInput.value = '';
-      
-      //moneyToSpend.value = parseFloat(moneyToSpend.innerText);
-      console.log(`This is money ${moneyToSpend.value}`);
+    addTask(newTask.value, newCost.value);
+
+
+    newCost.value.array.forEach(element => {
       
     });
-    
-  console.log(newTaskInput.value);
-  console.log(newCostInput.value);
-  
-  
-    // Function to add a new task
-    function addTask(taskText, costText) {
-      // Create new task element
-    //   const taskArray = [];
-      const costArray = [];
-      const taskItem = document.createElement('li');
-      //taskArray.push();
-      costArray.push();
-      taskItem.textContent = taskText;
-      //const totaltem = document.createElement('input');
 
-        console.log(taskItem);
+    newTask.value = ''; // Clear the input field
+    newCost.value = '';
 
+    //moneyToSpend.value = parseFloat(moneyToSpend.innerText);
+    console.log(`This is money ${moneyToSpend.value}`);
 
-      const costItem = document.createElement('li');
-     
-      costItem.textContent = costText;
-     costArray.push(costText);
-      console.log(costItem);
-      console.log(costArray);
-      
-     newAmountText.value = parseFloat(newAmountText.value ) + parseFloat(newCostInput.value);
-     console.log(`"T amount is" ${newAmountText.value}`);
-      // Add checkbox for completion
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.addEventListener('change', function () {
-        costItem.classList.toggle('completed');
-        // if (costItem.classList.style.display === "none"){   
-        //   console.log(`Este esta apagado ${costItem.classList.value}`)          
-        //  //(costItem.classList.toggle()){
-        // newAmountText.value -= parseFloat(costItem.classList.value);  
-        //if(Number(newAmountText.value) > Number(moneyToSpend.value)){
-
-
-      })
-      //costItem.classList.toggle('completed');
-
-       // addTotal(costArray, taskForm);
-      //});
-  
-      // Append elements to the task list
-      taskItem.appendChild(checkbox);
-      taskList.appendChild(taskItem);
-      taskList.appendChild(costItem);
-    }
-    moneyToSpend.addEventListener('change', function (event) {
-      calculateBalance();
-    });
-  
-    function calculateBalance() {
-      if(Number(newAmountText.value) > Number(moneyToSpend.value)){
-        appBlock.style.backgroundColor = "red";
-        notEnoughMoneyText.style.display = "block";
-      } else {
-        appBlock.style.backgroundColor = "#41dc0d";
-        notEnoughMoneyText.style.display = "none";
-      }
-    }
-
-
-
-
-
-
-
-
-
-
-
-    // function addTotal(arr, element){
-    //     if(arr != []){
-    //         summa = 0;
-    //         for (let i = 0; i < arr.length; i++){
-    //             summa += arr[i];
-    //         }
-    //         const total = document.createElement('input');
-    //         total.innerText = summa;
-    //         console.log(summa);
-    //     }
-    // }
-
-  
-    
   });
+
+
+  // Function to add a new task
+  function addTask(taskText, costText) {
+    // Create new task element
+    //   const taskArray = [];
+    const costArray = [];
+    const taskItem = document.createElement('li');
+    //taskArray.push();
+    costArray.push();
+    taskItem.textContent = taskText;
+    //const totaltem = document.createElement('input');
+
+    console.log(taskItem);
+
+    const costItem = document.createElement('li');
+
+    costItem.textContent = costText;
+    costArray.push(costText);
+    console.log(costItem);
+    console.log(costArray);
+
+    newAmountText.value = parseFloat(newAmountText.value) + parseFloat(costText);
+    console.log(`"T amount is" ${newAmountText.value}`);
+    // Add checkbox for completion
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('change', function (event) {
+
+      costItem.classList.toggle('completed');
+      const cost =
+        event.target.parentElement.parentElement.lastElementChild.textContent;
+      if (event.target.checked) {
+        newAmountText.value = parseFloat(newAmountText.value) - parseFloat(cost);
+      } else {
+        newAmountText.value = parseFloat(newAmountText.value) + parseFloat(cost);
+      }
+      calculateBalance();
+      // if (costItem.classList.style.display === "none"){
+      //   console.log(`Este esta apagado ${costItem.classList.value}`)
+      //  //(costItem.classList.toggle()){
+      // newAmountText.value -= parseFloat(costItem.classList.value);
+      //if(Number(newAmountText.value) > Number(moneyToSpend.value)){
+
+    })
+    calculateBalance();
+    //costItem.classList.toggle('completed');
+
+    // addTotal(costArray, taskForm);
+    //});
+
+    // Append elements to the task list
+    taskItem.appendChild(checkbox);
+    taskList.appendChild(taskItem);
+    taskList.appendChild(costItem);
+  }
+
+  moneyToSpend.addEventListener('change', function (event) {
+    calculateBalance();
+  });
+
+  function calculateBalance() {
+    if(Number(newAmountText.value) > Number(moneyToSpend.value)){
+      appBlock.style.backgroundColor = "red";
+      notEnoughMoneyText.style.display = "block";
+    } else {
+      appBlock.style.backgroundColor = "#41dc0d";
+      notEnoughMoneyText.style.display = "none";
+    }
+  }
+
+
+
+});
+
+  
+  
   
